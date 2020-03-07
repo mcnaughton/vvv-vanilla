b9cc21b766894a39ac5b1282604008beeb0ff565
Taylor Buley <buley@outlook.com>
2020-03-07 13:54:59 -0800

    fixes_init

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+cp -r www/mcnaughton-media/public_html/src/ www/mcnaughton-media/public_html/
+rm -rf www/mcnaughton-media/public_html/wp-content
 git clone git@github.com:wpcomvip/mcnaughton-media.git www/mcnaughton-media/public_html/wp-content
-git clone git@github.com:Automattic/vip-go-skeleton.git www/mcnaughton-media/public_html/wp-content/mu-plugins
+git clone git@github.com:Automattic/vip-go-mu-plugins.git www/mcnaughton-media/public_html/wp-content/mu-plugins
 
