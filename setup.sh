71e102cc5b1707638053520a9a4d0a5d07013270
Taylor Buley <buley@outlook.com>
2020-03-07 13:44:20 -0800

    develop init

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+git clone git@github.com:wpcomvip/mcnaughton-media.git www/mcnaughton-media/public_html/wp-content
+git clone git@github.com:Automattic/vip-go-skeleton.git www/mcnaughton-media/public_html/wp-content/mu-plugins
+
