0aef32447bee3064919c79badb923a6d70d29152
Taylor Buley <buley@outlook.com>
2020-03-07 22:49:36 -0800

    .

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 rm -rf public_html/wp-content
 git clone git@github.com:wpcomvip/mcnaughton-media.git public_html/wp-content
-git submodule update --init --recursive
 git clone git@github.com:Automattic/vip-go-mu-plugins.git public_html/wp-content/mu-plugins
-cd public_html/wp-content/mu-plugins
 git submodule update --init --recursive
 
